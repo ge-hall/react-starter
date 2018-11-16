2e8720e07f0288a76d92b29bd5d8a281c89b6e9a
ge-hall <Andrew.Ge-Hall@healthcarelogic.com.au>
2018-11-16 22:08:09 +1000

    using React 16.7.0-alpha.2 with sample Example component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-/* eslint-disable no-undef */
-// import React from 'react';
-import ReactDOM from 'react-dom';
 
+
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom';
+// eslint-disable-next-line no-unused-vars
+import Example from './components/example';
 
 const title = 'My Minimal React Webpack Babel Setup';
-console.log('My Minimal React Webpack Babel Setup');
-
 
 ReactDOM.render(
-  <div>{title}</div>,
+  <div>{title}
+  <Example></Example>
+  </div>,
   document.getElementById('app'),
 );
 
 
-module.hot.accept();
+// module.hot.accept();
